54fc3cbd02cf5d52cdbc1efcc85c37549f316bb4
kuba <kuba456@gmail.com>
2024-03-22 00:18:18 -0700

    Negligible difference compared to standatd single threaded - data copy?

--- main.cpp
+++ main.cpp
@@ -29,17 +29,29 @@ void MulAddLoop(const T* HWY_RESTRICT mul_array,
 
 int main(){
     using namespace std;
-    std::vector<float> v_mult(16, 2.0);
-    std::vector<float> v_add(16, 1.0);
+    std::vector<float> v_mult(100000000, 2.0);
+    std::vector<float> v_add(100000000, 1.0);
     std::iota(v_add.begin(), v_add.end(), 1);
-    std::vector<float> x_arr(16, 3.0);
+    std::vector<float> x_arr(100000000, 3.0);
 
     // for(auto i : v_mult) cout << i  << ' ';
+
+    {
     const double t0 = hwy::platform::Now();
     MulAddLoop(&v_mult[0], &v_add[0], x_arr.size(), &x_arr[0]);
     const double elapsed = hwy::platform::Now() - t0;
-    cout << "elapsed: " << elapsed <<endl;
-    for(auto i : x_arr) cout << i  << ' ';
+    cout << "elapsed simd: " << elapsed <<endl;
+    //for(auto i : x_arr) cout << i  << ' ';
+    }
+
+    {
+    const double t0 = hwy::platform::Now();
+    for(auto i=0; i < v_mult.size();i++){
+        x_arr[i] = v_mult[i] + v_add[i];
+    }
+    const double elapsed = hwy::platform::Now() - t0;
+    cout << "elapsed single: " << elapsed <<endl;
+    }
 
     std::cout << "DONE" << std::endl;
 }
