eeb3bb4a3d312dff0086ff45a90b9f9a4efde114
kuba <kuba456@gmail.com>
2024-06-17 23:49:45 -0700

    cleanup

--- dynamic_dispatch.cpp
+++ dynamic_dispatch.cpp
@@ -1,9 +1,10 @@
 // #include "dynamic_dispatch.h"
 
-#undef HWY_TARGET_INCLUDE             // prevents error when re-including
+#undef HWY_TARGET_INCLUDE  // prevents error when re-including
 // TODO with gcc using relative file path errors with file not found
 //      changing to absolute path compiles but crash on AVX2_simd
-#define HWY_TARGET_INCLUDE "dynamic_dispatch.cpp"  // add file relative to root, dont use __FILE__
+#define HWY_TARGET_INCLUDE \
+  "dynamic_dispatch.cpp"  // add file relative to root, dont use __FILE__
 
 // Generates code for each enabled target by re-including this source file.
 #include <hwy/foreach_target.h>  // IWYU pragma: keep
@@ -23,26 +24,25 @@ namespace hn = hwy::HWY_NAMESPACE;  // necessary to avoid ADL error
 using T = float;
 
 HWY_ATTR void MulAddLoop(const T* HWY_RESTRICT mul_array,
-                const T* HWY_RESTRICT add_array,
-                const size_t size, T* HWY_RESTRICT x_array) {
-    const hn::ScalableTag<T> d;
+                         const T* HWY_RESTRICT add_array, const size_t size,
+                         T* HWY_RESTRICT x_array) {
+  const hn::ScalableTag<T> d;
 
-    for (size_t i = 0; i < size; i += hn::Lanes(d)) {
-        const auto mul = hn::LoadU(d, mul_array + i);
-        const auto add = hn::LoadU(d, add_array + i);
+  for (size_t i = 0; i < size; i += hn::Lanes(d)) {
+    const auto mul = hn::LoadU(d, mul_array + i);
+    const auto add = hn::LoadU(d, add_array + i);
 
-        // hwy::PrintArray(x_array, size);
-        
-        auto x = hn::LoadU(d, x_array + i);
-        x = hn::Add(mul, add);
-        hn::StoreU(x, d, x_array + i);
-    }
+    // hwy::PrintArray(x_array, size);
+
+    auto x = hn::LoadU(d, x_array + i);
+    x = hn::Add(mul, add);
+    hn::StoreU(x, d, x_array + i);
+  }
 }
 
-} // namespace HWY_NAMESPACE
-} // namespace EXAMPLE_DYNAMIC
+}  // namespace HWY_NAMESPACE
+}  // namespace EXAMPLE_DYNAMIC
 // HWY_AFTER_NAMESPACE();
-
 
 // The table of pointers to the various implementations in HWY_NAMESPACE must
 // be compiled only once (foreach_target #includes this file multiple times).
@@ -55,14 +55,13 @@ HWY_EXPORT(MulAddLoop);
 using T = float;
 
 HWY_DLLEXPORT void CallMulAddLoop(const T* HWY_RESTRICT mul_array,
-                   const T* HWY_RESTRICT add_array,
-                   const size_t size, T* HWY_RESTRICT x_array){
-
-    // This must reside outside of HWY_NAMESPACE because it references (calls the
-    // appropriate one from) the per-target implementations there.
-    // For static dispatch, use HWY_STATIC_DISPATCH.
-    return HWY_DYNAMIC_DISPATCH(MulAddLoop)(mul_array, add_array, size, x_array);
+                                  const T* HWY_RESTRICT add_array,
+                                  const size_t size, T* HWY_RESTRICT x_array) {
+  // This must reside outside of HWY_NAMESPACE because it references (calls the
+  // appropriate one from) the per-target implementations there.
+  // For static dispatch, use HWY_STATIC_DISPATCH.
+  return HWY_DYNAMIC_DISPATCH(MulAddLoop)(mul_array, add_array, size, x_array);
 }
-} // namespace EXAMPLE_DYNAMIC
+}  // namespace EXAMPLE_DYNAMIC
 
 #endif  // HWY_ONCE
--- dynamic_dispatch.h
+++ dynamic_dispatch.h
@@ -6,8 +6,8 @@
 namespace EXAMPLE_DYNAMIC {
 using T = float;
 HWY_DLLEXPORT void CallMulAddLoop(const T* HWY_RESTRICT mul_array,
-                const T* HWY_RESTRICT add_array,
-                const size_t size, T* HWY_RESTRICT x_array);
-} // namespace BBB
+                                  const T* HWY_RESTRICT add_array,
+                                  const size_t size, T* HWY_RESTRICT x_array);
+}  // namespace EXAMPLE_DYNAMIC
 
-#endif // DYNAMIC_DIPATCH_H_
+#endif  // DYNAMIC_DIPATCH_H_
